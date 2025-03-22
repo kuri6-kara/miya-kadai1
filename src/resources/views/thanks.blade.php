363b4e4c7ad079f13dc5262a6a74ef530de1076e
miya <tukiya8@yahoo.co.jp>
2025-03-22 22:26:17 +0900

    サンクスページ

--- src/resources/views/thanks.blade.php
+++ src/resources/views/thanks.blade.php
@@ -5,13 +5,22 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Contact Form</title>
     <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/thanks.css') }}" />
 </head>
 
 <body>
-    
+    <div class="thanks__content">
+        <div class="thanks__heading">
+            <h2>お問い合わせありがとうございました</h2>
+        </div>
+        <div class="form__button">
+            <a class="form__button-submit" href="/">
+                HOME
+            <a>
+        </div>
+    </div>
 </body>
 
 </html>
