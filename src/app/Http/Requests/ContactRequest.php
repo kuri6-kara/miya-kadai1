0bff952d70611a89a905b6d9a375112006dcaae3
miya <tukiya8@yahoo.co.jp>
2025-03-28 00:26:19 +0900

    バリデーションとエラーメッセージ

--- src/app/Http/Requests/ContactRequest.php
+++ src/app/Http/Requests/ContactRequest.php
@@ -34,4 +34,20 @@ class ContactRequest extends FormRequest
             'detail' => ['required', 'string', 'max:120'],
         ];
     }
+
+    public function messages()
+    {
+        return [
+            'first_name.required' => '姓を入力してください',
+            'last_name.required' => '名を入力してください',
+            'gender.required' => '性別を選択してください',
+            'email.required' => 'メールアドレスを入力してください',
+            'email.email' => 'メールアドレスはメール形式で入力してください',
+            'tel.required' => '電話番号を入力してください',
+            'tel.digits:5' => '電話番号は5桁までの数字で入力してください',
+            'address.required' => '住所を入力してください',
+            'detail.required' => 'お問い合せ内容を入力してください',
+            'detail.max120' => 'お問合せ内容は120文字以内で入力してください',
+        ];
+    }
 }
