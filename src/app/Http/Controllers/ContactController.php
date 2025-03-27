c3c357e518448caa0c16b9882766cbb0963f22cf
miya <tukiya8@yahoo.co.jp>
2025-03-27 23:19:09 +0900

    バリデーションとエラーメッセージ

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\ContactRequest;
 use App\Models\Contact;
 
 class ContactController extends Controller
@@ -11,14 +11,14 @@ class ContactController extends Controller
     {
         return view('index');
     }
-    public function confirm(Request $request)
+    public function confirm(ContactRequest $request)
     {
-        $contact = $request->only(['first_name', 'last_name','gender', 'email', 'tel', 'address','building','detail']);
+        $contact = $request->only(['first_name', 'last_name','gender', 'email', 'tel', 'address', 'building', 'detail']);
         return view('confirm', compact('contact'));
     }
-    public function store(Request $request)
+    public function store(ContactRequest $request)
     {
-        $contact = $request->only(['first_name', 'last_name', 'gender', 'email', 'tel', 'address', 'building', 'detail']);
+        $contact = $request->only(['first_name', 'last_name', 'gender', 'email', 'tel', 'address', 'building','detail']);
         Contact::create($contact);
         return view('thanks');
     }
---
+++ src/app/Http/Requests/ContactRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ContactRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'gender' => ['required'],
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'tel' => ['required', 'numeric', 'regex:/^[a-zA-Z0-9]+$/', 'digits:5'],
+            'address' => ['required', 'string', 'max:255'],
+            'building' => ['nullable', 'string', 'max:255'],
+            'detail' => ['required', 'string', 'max:120'],
+        ];
+    }
+}
