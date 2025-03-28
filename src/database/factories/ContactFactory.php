8e86f800eb4ce205be8688c6cbe905936de23306
miya <tukiya8@yahoo.co.jp>
2025-03-28 22:53:47 +0900

    シーダーとファクトリ

---
+++ src/database/factories/ContactFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Contact;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ContactFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'first_name' => $this->faker->name,
+            'last_name' => $this->faker->name,
+            
+        ];
+    }
+}
