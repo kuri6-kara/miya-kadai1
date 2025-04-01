f6cfdd86e2a6f843ac9f919dd21f4f2078ef782f
miya <tukiya8@yahoo.co.jp>
2025-04-01 19:01:18 +0900

    バリデーションとエラーメッセージ

--- src/app/Http/Requests/CategoryRequest.php
+++ src/app/Http/Requests/CategoryRequest.php
@@ -24,7 +24,7 @@ class CategoryRequest extends FormRequest
     public function rules()
     {
         return [
-            'content' => ['required', 'in:商品のお届けについて, 商品の交換について, 商品トラブル, ショップへのお問い合わせ, その他'],
+            'content' => ['required'],
         ];
     }
 
--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -132,7 +132,6 @@
         <div class="form__group-content">
             <div class="form__input--text">
                 <select class="create-form__item-select" name="category_id">
-                    <option value="">選択してください</option>
                     @foreach ($categories as $category)
                     <option value="{{ $category['id']}}">{{ $category['content'] }}</option>
                     @endforeach
