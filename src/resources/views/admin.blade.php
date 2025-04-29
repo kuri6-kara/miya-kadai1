d51be9305e4e93e584c2f5581fc3e60fd49af38a
miya <tukiya8@yahoo.co.jp>
2025-04-30 08:05:55 +0900

    管理画面

--- src/resources/views/admin.blade.php
+++ src/resources/views/admin.blade.php
@@ -31,9 +31,12 @@
                         <!-- <option value="">男性</option>
                         <option value="">女性</option>
                         <option value="">その他</option> -->
-                        <option value="男性" {{ request('gender')=='男性' ? 'selected' : ''}}>男性</option>
+                        <!-- <option value="男性" {{ request('gender')=='男性' ? 'selected' : ''}}>男性</option>
                         <option value="女性" {{ request('gender')=='女性' ? 'selected' : ''}}>女性</option>
-                        <option value="その他" {{ request('gender')=='その他' ? 'selected' : ''}}>その他</option>
+                        <option value="その他" {{ request('gender')=='その他' ? 'selected' : ''}}>その他</option> -->
+                        <option value="1" @if( request('gender')==1 ) selected @endif>男性</option>
+                        <option value="2" @if( request('gender')==2 ) selected @endif>女性</option>
+                        <option value="3" @if( request('gender')==3 ) selected @endif>その他</option>
                     </select>
                 </div>
                 <div>
@@ -85,60 +88,60 @@
             <div class="modal__window"> -->
         <div class="modal" id="{{$contact->id}}">
             <a href="#!" class="modal-overlay"></a>
-                <div class="modal__inner">
-                    <div class="modal__content">
-                        <form action="/delete" method="post">
-                            @csrf
-                            <div class="modal-form__group">
-                                <label for="">お名前</label>
-                                <p>{{$contact->first_name}}{{$contact->last_name}}</p>
-                            </div>
+            <div class="modal__inner">
+                <div class="modal__content">
+                    <form action="/delete" method="post">
+                        @csrf
+                        <div class="modal-form__group">
+                            <label for="">お名前</label>
+                            <p>{{$contact->first_name}}{{$contact->last_name}}</p>
+                        </div>
 
-                            <div class="modal-form__group">
-                                <label for="">性別</label>
-                                <p>
-                                    @if($contact->gender == 1)
-                                    男性
-                                    @elseif($contact->gender == 2)
-                                    女性
-                                    @else
-                                    その他
-                                    @endif
-                                </p>
-                            </div>
+                        <div class="modal-form__group">
+                            <label for="">性別</label>
+                            <p>
+                                @if($contact->gender == 1)
+                                男性
+                                @elseif($contact->gender == 2)
+                                女性
+                                @else
+                                その他
+                                @endif
+                            </p>
+                        </div>
 
-                            <div class="modal-form__group">
-                                <label for="">メールアドレス</label>
-                                <p>{{$contact->email}}</p>
-                            </div>
+                        <div class="modal-form__group">
+                            <label for="">メールアドレス</label>
+                            <p>{{$contact->email}}</p>
+                        </div>
 
-                            <div class="modal-form__group">
-                                <label for="">電話番号</label>
-                                <p>{{$contact->tell}}</p>
-                            </div>
+                        <div class="modal-form__group">
+                            <label for="">電話番号</label>
+                            <p>{{$contact->tell}}</p>
+                        </div>
 
-                            <div class="modal-form__group">
-                                <label for="">住所</label>
-                                <p>{{$contact->address}}</p>
-                            </div>
+                        <div class="modal-form__group">
+                            <label for="">住所</label>
+                            <p>{{$contact->address}}</p>
+                        </div>
 
-                            <div class="modal-form__group">
-                                <label for="">お問い合わせの種類</label>
-                                <p>{{$contact->category->content}}</p>
-                            </div>
+                        <div class="modal-form__group">
+                            <label for="">お問い合わせの種類</label>
+                            <p>{{$contact->category->content}}</p>
+                        </div>
 
-                            <div class="modal-form__group">
-                                <label for="">お問い合わせ内容</label>
-                                <p>{{$contact->detail}}</p>
-                            </div>
-                            <input type="hidden" name="id" value="{{ $contact->id }}">
-                            <input type="submit" value="削除">
-                        </form>
-                    </div>
-                    <a href="#" class="modal__close">×</a>
+                        <div class="modal-form__group">
+                            <label for="">お問い合わせ内容</label>
+                            <p>{{$contact->detail}}</p>
+                        </div>
+                        <input type="hidden" name="id" value="{{ $contact->id }}">
+                        <input type="submit" value="削除">
+                    </form>
                 </div>
+                <a href="#" class="modal__close">×</a>
             </div>
-            @endforeach
+        </div>
+        @endforeach
     </table>
 </div>
 @endsection
