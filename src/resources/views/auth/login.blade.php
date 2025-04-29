45d1ee3ade357e3b4528b6b53b5f4c5d40547f88
miya <tukiya8@yahoo.co.jp>
2025-04-30 08:57:53 +0900

    ログインcss

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -5,53 +5,40 @@
 @endsection
 
 @section('nav')
-<ul class="header-nav">
-    <li class="header-nav__item">
-        <a class="header-nav__link" href="/register">register</a>
-    </li>
-</ul>
+<!-- <ul class="header-nav">
+    <li class="header-nav__item"> -->
+<a class="header-nav__link" href="/register">register</a>
+<!-- </li>
+</ul> -->
 @endsection
 
 @section('content')
-<div class="login-form__content">
-    <div class="login-form__heading">
-        <h2>Login</h2>
-    </div>
-    <form class="form" action="/login" method="post">
-        @csrf
-        <div class="form__group">
-            <div class="form__group-title">
-                <span class="form__label--item">メールアドレス</span>
-            </div>
-            <div class="form__group-content">
-                <div class="form__input--text">
-                    <input type="email" name="email" value="{{ old('email') }}" />
-                </div>
-                <div class="form__error">
+<div class="login-form">
+    <h2 class="login-form__heading content__heading">Login</h2>
+    <div class="login-form__inner">
+        <form class="login-form__form" action="/login" method="post">
+            @csrf
+            <div class="login-form__group">
+                <span class="login-form__label">メールアドレス</span>
+                <input class="login-form__input" type="email" name="email" value="{{ old('email') }}" />
+                <div class="register-form__error-message">
                     @error('email')
                     {{ $message }}
                     @enderror
                 </div>
             </div>
-        </div>
-        <div class="form__group">
-            <div class="form__group-title">
-                <span class="form__label--item">パスワード</span>
-            </div>
-            <div class="form__group-content">
-                <div class="form__input--text">
-                    <input type="password" name="password" />
-                </div>
+            <div class="login-form__group">
+                <span class="login-form__label">パスワード</span>
+                <input class="login-form__input" type="password" name="password" />
                 <div class="form__error">
                     @error('password')
                     {{ $message }}
                     @enderror
                 </div>
             </div>
-        </div>
-        <div class="form__button">
-            <button class="form__button-submit" type="submit">ログイン</button>
-        </div>
-    </form>
-</div>
-@endsection
+            <div class="form__button">
+                <button class="form__button-submit" type="submit">ログイン</button>
+            </div>
+        </form>
+    </div>
+    @endsection
